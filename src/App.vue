a057f74f520f383cba8f0d1fa406ff98e52bb98e
NoudH <noudhabraken@gmail.com>
2021-12-27 20:48:13 +0100

    PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: 'App',
@@ -15,12 +14,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/arya-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css';
+import ScrollPanel from 'primevue/scrollpanel';
+import ProgressSpinner from 'primevue/progressspinner';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {ripple: true});
+
+app.component("ScrollPanel", ScrollPanel);
+app.component("ProgressSpinner", ProgressSpinner);
+
+app.mount('#app');
